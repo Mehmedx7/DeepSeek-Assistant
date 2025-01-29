78f634b8d936300ace17ce2df6468e1887f10e28
Mohammed <mehmedkhan16@gmail.com>
2025-01-29 20:12:47 +0530

    feat: bot added

--- app/main.py
+++ app/main.py
@@ -39,3 +39,9 @@ async def chat_endpoint(data: dict):
 @app.get("/")
 def serve_homepage(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 9000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
